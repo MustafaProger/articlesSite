a68de2c267322e28c30132da82c983fc55104dbf
DIOR <diertojiev@yandex.ru>
2023-07-26 21:05:16 +0300

    Большие изменения сайта
    1. Изменение расположение header__list 
    2. Добавление инонки вместо заголовка 
    3. Добавление выпадающего меню контакты 
    4. Добавление формы form search
    5. Изменения черного цвета на #2b3035

--- src/js/index.js
+++ src/js/index.js
@@ -18,46 +18,10 @@
     
 // }
 
-
-// window.addEventListener('scroll', function() {
-//     const header = document.querySelector('.header');
-//     const hyperlink__1 = document.querySelector('#hyperlink__1');
-//     const hyperlink__2 = document.querySelector('#hyperlink__2');
-//     const hyperlink__3 = document.querySelector('#hyperlink__3');
-//     const hyperlink__4 = document.querySelector('#hyperlink__4');
-//     const hyperlink__5 = document.querySelector('#hyperlink__5');
-//     const header__logo = document.querySelector('#header__logo');
-//     const header__burger = document.querySelector('.header__burger');
-//     const scrolled = window.scrollY > 0;
-  
-//     if(scrolled) {
-//         header.classList.add('scrolled');
-//         hyperlink__1.classList.add('scrolled');
-//         hyperlink__2.classList.add('scrolled');
-//         hyperlink__3.classList.add('scrolled');
-//         hyperlink__4.classList.add('scrolled');
-//         hyperlink__5.classList.add('scrolled');
-//         header__logo.classList.add('scrolled');
-//         header__burger.classList.add('scrolled');
-
-//     } else {
-//         header.classList.remove('scrolled');
-//         hyperlink__1.classList.remove('scrolled');
-//         hyperlink__2.classList.remove('scrolled');
-//         hyperlink__3.classList.remove('scrolled');
-//         hyperlink__4.classList.remove('scrolled');
-//         hyperlink__5.classList.remove('scrolled');
-//         header__logo.classList.remove('scrolled');
-//         header__burger.classList.remove('scrolled');
-//     }
-// });
-
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
         $(".header__burger").toggleClass('active');
         $(".header__menu").toggleClass('active');
         $("body").toggleClass('lock')
-
     });
 });
-
